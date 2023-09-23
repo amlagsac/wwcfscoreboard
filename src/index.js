d40242faa946062157a356f98bcad5b25dd2c313
Andre Lagsac <andremlagsac@gmail.com>
2023-09-23 15:58:56 +0800

    Revert "Edit Router"
    This reverts commit 5b35c3650f35712259aca6d4a541313e844a5c87.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Scoreboard from './Scoreboard';
 import './App.css';
-import {Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path='/' exact element={ <App/> }></Route>
           <Route path='/scoreboard' element={ <Scoreboard /> } />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
   </React.StrictMode>
 );
